e8d92f27e8648d6afcc7484cf12b4d2f3fdd1be7
nessa-waithaka <vanessa.waithaka@gmail.com>
2019-11-01 15:52:01 +0300

    Adding js to forms

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,40 @@
+$(document).ready(function() {
+  $("#contacts form").submit(function(event) {
+    var fullname = $("input#name").val();
+    var email = $("input#mail").val();
+    var text = $("textarea#textarea").val();
 
-$('#design').click(function () {
-    $('#design p').toggle();
-    $('#design img').toggle();
+    $(".name").text(fullname);
+    $(".mail").text(email);
+    $(".text").text(text);
+
+    if (fullname == "" || email == "") {
+      alert("Please fill in all your details");
+    } else if (text == "") {
+      alert("Kindly write a message");
+    } else {
+      alert(
+        fullname +
+          " we have received your message. Thank you for reaching out to us."
+      );
+    }
+
+    event.preventDefault();
+  });
 });
 
-$('#dev').click(function () {
-    $('#dev p').toggle();
-    $('#dev img').toggle();
+//Toggle function
+$("#design").click(function() {
+  $("#design p").toggle();
+  $("#design img").toggle();
 });
 
-$('#product').click(function () {
-    $('#product p').toggle();
-    $('#product img').toggle();
+$("#dev").click(function() {
+  $("#dev p").toggle();
+  $("#dev img").toggle();
+});
+
+$("#product").click(function() {
+  $("#product p").toggle();
+  $("#product img").toggle();
 });
