d51f93f9de10b7d08de3f0a62fc574c6a9c8dd8e
nessa-waithaka <vanessa.waithaka@gmail.com>
2019-11-03 17:19:53 +0300

    Add project names to portfolio

--- js/scripts.js
+++ js/scripts.js
@@ -38,3 +38,28 @@ $("#product").click(function() {
   $("#product p").toggle();
   $("#product img").toggle();
 });
+
+$(".col-md-3 #w1").hover(function() {
+  $("#p1").toggle();
+});
+$(".col-md-3 #w2").hover(function() {
+  $("#p2").toggle();
+});
+$(".col-md-3 #w3").hover(function() {
+  $("#p3").toggle();
+});
+$(".col-md-3 #w4").hover(function() {
+  $("#p4").toggle();
+});
+$(".col-md-3 #w5").hover(function() {
+  $("#p5").toggle();
+});
+$(".col-md-3 #w6").hover(function() {
+  $("#p6").toggle();
+});
+$(".col-md-3 #w7").hover(function() {
+  $("#p7").toggle();
+});
+$(".col-md-3 #w8").hover(function() {
+  $("#p8").toggle();
+});
